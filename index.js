85d806cd6d22646f67d58886f4608f5f3980ae54
avinashvidyarthi <avinashvidyarthi@gmail.com>
2022-08-18 23:13:24 +0530

    basic express

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const cors = require('cors');
+const port = process.env.PORT || 3000;
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(cors());
+
+app.get('/', (req, res) => {
+	res.send('OK');
+});
+
+app.post('/notification', (req, res) => {
+	console.log(req.body);
+	res.send('OKKK');
+});
+
+app.listen(port, () => {
+	console.log(`Server is listening on port: ${port}`);
+});
